ba2732db7ade21147443e53b8099c835b2abb135
plenilune3 <alsrb1973@naver.com>
2019-04-18 22:32:14 +0900

    exam7

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -12,21 +12,32 @@ public class UserDao {
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
 
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("select * from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
 
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+            user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
         //리턴
         return user;
 
@@ -34,24 +45,56 @@ public class UserDao {
 
     public Long add(User user) throws SQLException, ClassNotFoundException {
 
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        Long id;
 
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("insert into userinfo(name, password) values(?, ?)");
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
+        try {
+            connection = dataSource.getConnection();
 
-        preparedStatement.executeUpdate();
+            preparedStatement = connection.prepareStatement("insert into userinfo(name, password) values(?, ?)");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
 
-        preparedStatement = connection.prepareStatement("select last_insert_id()");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
+            id = getLastInsertId(connection, preparedStatement);
+        } finally {
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
 
-        Long id = resultSet.getLong(1);
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        return id;
+    }
+
+    public Long getLastInsertId(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+        ResultSet resultSet = null;
+        Long id;
+        try {
+            preparedStatement.executeUpdate();
+            PreparedStatement preparedStatement1 = connection.prepareStatement("select last_insert_id()");
+            resultSet = preparedStatement1.executeQuery();
+            resultSet.next();
+
+            id = resultSet.getLong(1);
+        } finally {
+            if (resultSet != null){
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         return id;
     }
